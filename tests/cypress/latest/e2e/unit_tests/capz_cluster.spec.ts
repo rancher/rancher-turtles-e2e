3492da66a18c905b0795a1203783c3a57cd5d6d5
Parthvi Vala <parthvi.vala@suse.com>
2024-09-02 19:56:05 +0530

    Add manifest files
    Signed-off-by: Parthvi Vala <parthvi.vala@suse.com>

---
+++ tests/cypress/latest/e2e/unit_tests/capz_cluster.spec.ts
@@ -0,0 +1,85 @@
+import '~/support/commands';
+import * as cypressLib from '@rancher-ecp-qa/cypress-library';
+import { qase } from 'cypress-qase-reporter/dist/mocha';
+
+Cypress.config();
+describe('Import CAPA', () => {
+  const timeout = 1200000
+  const repoName = 'clusters'
+  const clusterName = "turtles-qa-cluster"
+  const branch = 'main'
+  const path = '/tests/assets/rancher-turtles-fleet-example/azure'
+  const repoUrl = "https://github.com/rancher/rancher-turtles-e2e.git"
+
+  beforeEach(() => {
+    cy.login();
+    cypressLib.burgerMenuToggle();
+  });
+
+  it('Setup the namespace for importing', () => {
+    cy.namespaceAutoImport('Enable');
+  })
+
+  qase(14,
+    it('Add CAPA cluster fleet repo', () => {
+      cypressLib.checkNavIcon('cluster-management')
+        .should('exist');
+
+      // Add CAPA fleet repository
+      cy.addFleetGitRepo(repoName, repoUrl, branch, path);
+      cy.contains(repoName).click();
+
+      // Go to Cluster Management > CAPI > Clusters and check if the cluster has started provisioning
+      cypressLib.burgerMenuToggle();
+      cy.checkCAPIMenu();
+      cy.contains('Provisioned ' + clusterName, { timeout: timeout });
+    })
+  );
+
+  it('Auto import child CAPA cluster', () => {
+    // Check child cluster is created and auto-imported
+    cy.goToHome();
+    cy.contains('Pending ' + clusterName);
+
+    // Check cluster is Active
+    cy.clickButton('Manage');
+    cy.contains('Active ' + clusterName, { timeout: 300000 });
+  })
+
+  it('Install App on imported cluster', { retries: 1 }, () => {
+    // Click on imported CAPA cluster
+    cy.contains(clusterName).click();
+
+    // Install App
+    cy.installApp('Monitoring', 'cattle-monitoring');
+  })
+
+  qase(15,
+    it('Remove imported CAPA cluster from Rancher Manager', { retries: 1 }, () => {
+
+      // Check cluster is not deleted after removal
+      cy.deleteCluster(clusterName);
+      cy.goToHome();
+      // kubectl get clusters.cluster.x-k8s.io
+      // This is checked by ensuring the cluster is not available in navigation menu
+      cy.contains(clusterName).should('not.exist');
+      cy.checkCAPIClusterProvisioned(clusterName);
+    })
+  );
+
+  qase(16,
+    it('Delete the CAPA cluster fleet repo', () => {
+
+      // Remove the fleet git repo
+      cy.removeFleetGitRepo(repoName)
+      // Wait until the following returns no clusters found
+      // This is checked by ensuring the cluster is not available in CAPI menu
+      cypressLib.burgerMenuToggle();
+      cy.checkCAPIMenu();
+      cy.getBySel('button-group-child-1').click();
+      cy.typeInFilter(clusterName);
+      cy.getBySel('sortable-table-0-action-button', { timeout: timeout }).should('not.exist');
+    })
+  );
+
+});
