a0c6c998625c517dbb02364cbbcffa49fef5231b
Parthvi Vala <parthvi.vala@suse.com>
2025-07-02 15:31:04 +0530

    Review comments and add qase ids
    Co-authored-by: Chandan Pinjani <chandan.pinjani@suse.com>
    Signed-off-by: Parthvi Vala <parthvi.vala@suse.com>

--- tests/cypress/latest/e2e/unit_tests/capg_kubeadm_clusterclass.spec.ts
+++ tests/cypress/latest/e2e/unit_tests/capg_kubeadm_clusterclass.spec.ts
@@ -4,7 +4,7 @@ import { qase } from 'cypress-qase-reporter/dist/mocha';
 import { skipClusterDeletion } from '~/support/utils';
 
 Cypress.config();
-describe('Import CAPG GKE Cluster', { tags: '@full' }, () => {
+describe('Import CAPG Kubeadm Cluster', { tags: '@full' }, () => {
   var clusterName: string
   const timeout = 1200000
   const className = 'gcp-kubeadm-example'
@@ -41,39 +41,43 @@ describe('Import CAPG GKE Cluster', { tags: '@full' }, () => {
   })
 
   it('Setup the namespace for importing', () => {
-    cy.namespaceAutoImport('Enable');
+    cy.namespaceAutoImport('Disable');
   })
 
-  it('Add CAPG Kubeadm ClusterClass Fleet Repo and check GCP CCM', () => {
-    cy.addFleetGitRepo(clusterClassRepoName, turtlesRepoUrl, 'main', classesPath, 'capi-classes')
-    // Go to CAPI > ClusterClass to ensure the clusterclass is created
-    cy.checkCAPIClusterClass(className);
+  qase(148,
+    it('Add CAPG Kubeadm ClusterClass Fleet Repo and check GCP CCM', () => {
+      cy.addFleetGitRepo(clusterClassRepoName, turtlesRepoUrl, 'main', classesPath, 'capi-classes')
+      // Go to CAPI > ClusterClass to ensure the clusterclass is created
+      cy.checkCAPIClusterClass(className);
 
-    // Navigate to `local` cluster, More Resources > Fleet > Helm Apps and ensure the charts are active.
-    cy.burgerMenuOperate('open');
-    cy.contains('local').click();
-    cy.accesMenuSelection(['More Resources', 'Fleet', 'HelmApps']);
-    cy.typeInFilter("calico-cni");
-    cy.getBySel('sortable-cell-0-1').should('exist');
-    cy.accesMenuSelection(['More Resources', 'Fleet', 'Bundle']);
-    cy.typeInFilter("cloud-controller-manager-gcp");
-    cy.getBySel('sortable-cell-0-1').should('exist');
+      // Navigate to `local` cluster, More Resources > Fleet > Helm Apps and ensure the charts are active.
+      cy.burgerMenuOperate('open');
+      cy.contains('local').click();
+      cy.accesMenuSelection(['More Resources', 'Fleet', 'HelmApps']);
+      cy.typeInFilter("calico-cni");
+      cy.getBySel('sortable-cell-0-1').should('exist');
+      cy.accesMenuSelection(['More Resources', 'Fleet', 'Bundle']);
+      cy.typeInFilter("cloud-controller-manager-gcp");
+      cy.getBySel('sortable-cell-0-1').should('exist');
 
-  });
+    })
+  );
 
-  it('Add GitRepo for class-cluster and get cluster name', () => {
-    cy.addFleetGitRepo(repoName, repoUrl, branch, path);
-    // Check CAPI cluster using its name prefix i.e. className
-    cy.checkCAPICluster(className);
+  qase(143,
+    it('Add GitRepo for class-cluster and get cluster name', () => {
+      cy.addFleetGitRepo(repoName, repoUrl, branch, path);
+      // Check CAPI cluster using its name prefix i.e. className
+      cy.checkCAPICluster(className);
 
-    // Get the cluster name by its prefix and use it across the test
-    cy.getBySel('sortable-cell-0-1').then(($cell) => {
-      clusterName = $cell.text();
-      cy.log('CAPI Cluster Name:', clusterName);
-    });
-  });
+      // Get the cluster name by its prefix and use it across the test
+      cy.getBySel('sortable-cell-0-1').then(($cell) => {
+        clusterName = $cell.text();
+        cy.log('CAPI Cluster Name:', clusterName);
+      });
+    })
+  );
 
-  qase(36,
+  qase(144,
     it('Auto import child CAPG cluster', () => {
       // Go to Cluster Management > CAPI > Clusters and check if the cluster has provisioned
       cy.checkCAPIClusterProvisioned(clusterName, timeout);
@@ -86,10 +90,13 @@ describe('Import CAPG GKE Cluster', { tags: '@full' }, () => {
       // Check cluster is Active
       cy.searchCluster(clusterName);
       cy.contains(new RegExp('Active.*' + clusterName), { timeout: timeout });
+      // Go to Cluster Management > CAPI > Clusters and check if the cluster has provisioned
+      // Ensuring cluster is provisioned also ensures all the Cluster Management > Advanced > Machines for the given cluster are Active.
+      cy.checkCAPIClusterActive(clusterName, timeout);
     })
   );
 
-  qase(37,
+  qase(145,
     it('Install App on imported cluster', () => {
       // Click on imported CAPG cluster
       cy.contains(clusterName).click();
@@ -101,7 +108,7 @@ describe('Import CAPG GKE Cluster', { tags: '@full' }, () => {
   );
 
   if (skipClusterDeletion) {
-    qase(38,
+    qase(146,
       it('Remove imported CAPG cluster from Rancher Manager', { retries: 1 }, () => {
 
         // Check cluster is not deleted after removal
@@ -114,7 +121,7 @@ describe('Import CAPG GKE Cluster', { tags: '@full' }, () => {
       })
     );
 
-    qase(39,
+    qase(147,
       it('Delete the CAPG cluster fleet repo', () => {
 
         // Remove the fleet git repo
@@ -130,6 +137,6 @@ describe('Import CAPG GKE Cluster', { tags: '@full' }, () => {
 
     it('Delete the helm values secret', () => {
       cy.deleteKubernetesResource('local', ['More Resources', 'Core', 'Secrets'], "capg-helm-values", namespace)
-    })
+    });
   }
 });
