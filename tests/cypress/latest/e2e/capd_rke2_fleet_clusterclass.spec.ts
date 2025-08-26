18628577ada819eec67db73f8ba1d7488985af10
Tomas Hehejik <thehejik@suse.com>
2025-08-26 19:57:55 +0200

    Make docker auth secrets optional

--- tests/cypress/latest/e2e/capd_rke2_fleet_clusterclass.spec.ts
+++ tests/cypress/latest/e2e/capd_rke2_fleet_clusterclass.spec.ts
@@ -28,8 +28,8 @@ describe('Import CAPD RKE2 Class-Cluster', { tags: '@short' }, () => {
   const classesPath = 'examples/clusterclasses/docker/rke2'
   const clustersRepoName = 'docker-rke2-class-clusters'
   const clusterClassRepoName = "docker-rke2-clusterclass"
-  const dockerAuthUsernameBase64 = btoa(Cypress.env("docker_auth_username"))
-  const dockerAuthPasswordBase64 = btoa(Cypress.env("docker_auth_password"))
+  const dockerAuthUsernameBase64 = btoa(Cypress.env("docker_auth_username") || "")
+  const dockerAuthPasswordBase64 = btoa(Cypress.env("docker_auth_password") || "")
   const capiClustersNS = 'capi-clusters'
 
   beforeEach(() => {
@@ -43,11 +43,13 @@ describe('Import CAPD RKE2 Class-Cluster', { tags: '@short' }, () => {
 
   it('Create Docker Auth Secret', () => {
     // Prevention for Docker.io rate limiting
-    cy.readFile('./fixtures/docker/capd-auth-token-secret.yaml').then((data) => {
-      data = data.replace(/replace_cluster_docker_auth_username/, dockerAuthUsernameBase64)
-      data = data.replace(/replace_cluster_docker_auth_password/, dockerAuthPasswordBase64)
-      cy.importYAML(data, capiClustersNS)
-    })
+    if (dockerAuthUsernameBase64) {
+      cy.readFile('./fixtures/docker/capd-auth-token-secret.yaml').then((data) => {
+        data = data.replace(/replace_cluster_docker_auth_username/, dockerAuthUsernameBase64)
+        data = data.replace(/replace_cluster_docker_auth_password/, dockerAuthPasswordBase64)
+        cy.importYAML(data, capiClustersNS)
+      })
+    }
   });
 
   it('Add CAPD RKE2 ClusterClass Fleet Repo', () => {
