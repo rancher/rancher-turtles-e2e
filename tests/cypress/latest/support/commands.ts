be0e908f8183a6eba3079aa713cb6e107343c972
Tomas Hehejik <thehejik@suse.com>
2025-09-09 18:16:37 +0200

    Press Enter in typeInFilter()

--- tests/cypress/latest/support/commands.ts
+++ tests/cypress/latest/support/commands.ts
@@ -742,7 +742,7 @@ Cypress.Commands.add('typeInFilter', (text, selector = '.input-sm') => {
   cy.get(selector)
     .click()
     .clear()
-    .type(text)
+    .type(text + '{enter}')
     .wait(2000);
 });
 
