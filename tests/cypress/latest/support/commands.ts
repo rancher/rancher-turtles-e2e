837b5fc210da03abd94c413dee4b27e7bee16757
Parthvi Vala <parthvi.vala@suse.com>
2025-09-19 18:15:03 +0530

    Update tests/cypress/latest/support/commands.ts
    Co-authored-by: Tomas Hehejik <thehejik@suse.com>

--- tests/cypress/latest/support/commands.ts
+++ tests/cypress/latest/support/commands.ts
@@ -545,7 +545,7 @@ Cypress.Commands.add('checkChart', (operation, chartName, namespace, version, qu
     cy.typeInFilter(chartName);
   }
   let chartSelector = isRancherManagerVersion('>=2.12') ? 'app-chart-cards-container' : 'chart-selection-grid';
-  const turtlesChartSelector = isRancherManagerVersion('2.12') ? '"item-card-cluster/turtles-chart/rancher-turtles"' : '"select-icon-grid-Rancher Turtles - the Cluster API Extension"';
+  const turtlesChartSelector = isRancherManagerVersion('>=2.12') ? '"item-card-cluster/turtles-chart/rancher-turtles"' : '"select-icon-grid-Rancher Turtles - the Cluster API Extension"';
   
   if (chartName == 'Rancher Turtles') {
     chartSelector = turtlesChartSelector
