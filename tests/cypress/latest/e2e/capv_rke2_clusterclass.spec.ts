2d8d665d963e10f9dd88d11e29b704e891967cb4
Parthvi Vala <parthvi.vala@suse.com>
2026-02-06 20:46:46 +0530

    v1beta1 > v1beta2 for tests using fleet
    Signed-off-by: Parthvi Vala <parthvi.vala@suse.com>

--- tests/cypress/latest/e2e/capv_rke2_clusterclass.spec.ts
+++ tests/cypress/latest/e2e/capv_rke2_clusterclass.spec.ts
@@ -1,7 +1,7 @@
 import '~/support/commands';
 import * as cypressLib from '@rancher-ecp-qa/cypress-library';
 import {qase} from 'cypress-qase-reporter/mocha';
-import {isRancherManagerVersion, skipClusterDeletion} from '~/support/utils';
+import {isAPIv1beta1, isRancherManagerVersion, skipClusterDeletion} from '~/support/utils';
 import {capiClusterDeletion, capvResourcesCleanup, importedRancherClusterDeletion} from "~/support/cleanup_support";
 import {vars} from '~/support/variables';
 
@@ -12,7 +12,10 @@ describe('Import CAPV RKE2 Class-Cluster', {tags: '@vsphere'}, () => {
   const classRepoName = 'vsphere-rke2-clusterclass'
   const className = 'vsphere-rke2-example'
   const clusterName = 'turtles-qa-capv-rke2-example'
-  const path = '/tests/assets/rancher-turtles-fleet-example/capv/rke2/class-clusters'
+  let path = '/tests/assets/rancher-turtles-fleet-example/capv/rke2/class-clusters'
+  if (isAPIv1beta1) {
+    path = '/tests/assets/rancher-turtles-fleet-example/capv/rke2/class-clusters-v1beta1'
+  }
   const classesPath = 'examples/clusterclasses/vsphere/rke2'
   const vsphere_secrets_json_base64 = Cypress.env("vsphere_secrets_json_base64")
   const providerName = 'vsphere'
