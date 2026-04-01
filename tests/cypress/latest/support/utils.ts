f625749082d9b8f831bb6776c63a5eedd4ea76c6
Parthvi Vala <parthvi.vala@suse.com>
2026-04-01 15:09:20 +0530

    Revert provider chart name on all except 2.13-alpha|-rc builds
    Signed-off-by: Parthvi Vala <parthvi.vala@suse.com>

--- tests/cypress/latest/support/utils.ts
+++ tests/cypress/latest/support/utils.ts
@@ -44,6 +44,14 @@ export const providersChartNeedsStgRegistry = (): boolean => {
   return (!isTurtlesDevChart) && (isPreRelease)
 }
 
+// Check if Rancher Turtles Providers chart should use staging registry chart name
+// Returns true for Rancher 2.13 alpha/rc builds
+// TODO: Remove this once https://github.com/rancher/rancher/issues/53882 and 53883 is fixed; staging registry is currently broken for everything
+export const needsProvidersStgChartName = (): boolean => {
+  return isRancherManagerVersion('2.13') &&
+    /(-alpha|-rc)/.test(rancherVersion)
+}
+
 export const isTurtlesPrimeBuild = (): boolean =>{
   return Cypress.expose("turtles_build_type") === "prime";
 }
