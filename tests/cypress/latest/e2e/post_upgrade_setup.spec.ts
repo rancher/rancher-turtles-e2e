5169fd59793536ade7fc1115adff27c46f56880e
Chandan Pinjani <chandan.pinjani@suse.com>
2026-08-06 20:01:57 +0530

    Update upgrade version
    Signed-off-by: Chandan Pinjani <chandan.pinjani@suse.com>

--- tests/cypress/latest/e2e/post_upgrade_setup.spec.ts
+++ tests/cypress/latest/e2e/post_upgrade_setup.spec.ts
@@ -5,6 +5,7 @@ import {isTurtlesDevChart, turtlesNamespace} from '../support/utils';
 Cypress.config();
 describe('Post Rancher Upgrade Setup - @upgrade', {tags: '@upgrade'}, () => {
   const timeout = vars.shortTimeout
+  const localCluster = 'local'
 
   beforeEach(() => {
     cy.login();
@@ -13,19 +14,25 @@ describe('Post Rancher Upgrade Setup - @upgrade', {tags: '@upgrade'}, () => {
 
   qase(510, it('Check the local cluster status is active post-upgrade', ()=>{
     // Check local cluster is Active
-    const localCluster = 'local'
     cy.searchCluster(localCluster);
     cy.contains(new RegExp('Active.*' + localCluster), {timeout: timeout});
   })
   );
 
   qase(511, it('Check upgraded Turtles chart', () => {
+    cy.exploreCluster(localCluster);
+    cy.setNamespace('cattle-system');
+    cy.clickNavMenu(['Apps', 'Installed Apps']);
+    cy.typeInFilter('rancher');
+    cy.getBySel('sortable-cell-0-1').should('exist');
+    cy.getBySel('sortable-cell-0-3').contains('2.14', {timeout: timeout});
+    cy.waitForAllRowsInState('Deployed', timeout);
+
     cy.exploreCluster('local');
     cy.setNamespace(turtlesNamespace);
     cy.clickNavMenu(['Apps', 'Installed Apps']);
     cy.typeInFilter('rancher-turtles');
     cy.getBySel('sortable-cell-0-1').should('exist');
-
     const turtlesChartVersion = isTurtlesDevChart? Cypress.expose('turtles_chart_dev_version'): '0.26'
     cy.getBySel('sortable-cell-0-3').contains(turtlesChartVersion, {timeout: timeout});
     cy.waitForAllRowsInState('Deployed', timeout);
