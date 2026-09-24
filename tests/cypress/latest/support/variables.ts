c4934ff843cd4616c40263e116858eb196746d74
Chandan Pinjani <chandan.pinjani@suse.com>
2026-09-24 14:08:51 +0530

    Modify AWS AMI
    Signed-off-by: Chandan Pinjani <chandan.pinjani@suse.com>

--- tests/cypress/latest/support/variables.ts
+++ tests/cypress/latest/support/variables.ts
@@ -41,9 +41,9 @@ export const vars = {
   v2provRKE2Version: isRancherManagerVersion('2.12') ? 'v1.33.4+rke2r1' : isRancherManagerVersion('2.13') ? 'v1.34.10+rke2r1' : isRancherManagerVersion('2.14') ? 'v1.35.7+rke2r1' : 'v1.36.3+rke2r1',
   v2provImageId: isRancherManagerVersion('2.12') ? 'canonical:UbuntuServer:18.04-LTS:latest' : 'canonical:ubuntu-24_04-lts:server-gen1:latest',
   amiID: isRancherManagerVersion('2.12') ? 'ami-07cded2dd011bc687' // Private copy of ami-0cd9e4e7906f4c9dd from eu-west-2
-  : isRancherManagerVersion('2.13') ? 'ami-010b4d392889007a3' // Private copy of ami-055123d49b91c2827 from eu-west-2
-  : isRancherManagerVersion('2.14') ? 'ami-0da7e3e1c75ab13ab' // Private copy of ami-0bb0dc2c3c4dbf68f from eu-west-2
-  : 'ami-05402dfd155c256a5', // Private copy of ami-032938cfb36a7f7ce from eu-west-2
+  : isRancherManagerVersion('2.13') ? 'ami-01c17389b5b9ad5f2' // Private copy of ami-055123d49b91c2827 from eu-west-2
+  : isRancherManagerVersion('2.14') ? 'ami-080efa71f688839e1' // Private copy of ami-0bb0dc2c3c4dbf68f from eu-west-2
+  : 'ami-05984e6a3f84cb993', // Private copy of ami-032938cfb36a7f7ce from eu-west-2
   gcpImageId: isRancherManagerVersion('2.12')
   ? 'cluster-api-ubuntu-2404-v1-33-5-1762252437'
   : isRancherManagerVersion('2.13') || isRancherManagerVersion('2.14')
